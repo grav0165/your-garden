d5c7df6728394c0dccbc4ef13b113dec0ca7a489
Aubrey Graves <grav0165@gmail.com>
2023-07-31 11:13:14 -0500

    Debugging the GET request to the API

--- src/redux/sagas/api.saga.js
+++ src/redux/sagas/api.saga.js
@@ -4,7 +4,7 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* fetchApi () {
     try {
         const perenualResponse = yield axios.get(`/api/perenual/details`)
-        const perenualDetailResponse = yield axios(`/api/careDetails`)
+        const perenualDetailResponse = yield axios.get(`/api/careDetails`)
         yield put({ type: 'SET_API_RESULT', payload: {base: perenualResponse.data, care: perenualDetailResponse.data}})
     } catch (error) {
         console.log('API get request failed', error);
