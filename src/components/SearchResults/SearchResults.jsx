96891f4eb86e24a76955ab65d945d8f1c59e4df2
Aubrey Graves <grav0165@gmail.com>
2023-07-31 14:34:20 -0500

    Created a path for default plant images incase the API does not have any images on file

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+
+
 // importing MUI
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
@@ -10,40 +12,65 @@ import CardActionArea from "@mui/material/CardActionArea";
 
 // importing store 
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 
-function searchResults () {
+function searchResults() {
 
-    const apiSearchResult = useSelector( store  => store.api.apiSearchResponse)
-
-    const handleStore = () => {
-         console.log('apiSearchResult is the following array: ', apiSearchResult)
-    }
+    const apiSearchResult = useSelector(store => store.api.apiSearchResponse)
    
 
+    const handleStore = () => {
+        console.log('apiSearchResult is the following array: ', apiSearchResult)
+    }
+
+    // Created a variable thats conditional to avoid broken links
+    let image;
+
+   const plantImage = (plant) => {
+        if (plant?.default_image == null) {
+            image = "images/default.png";
+        } else if (plant?.default_image?.original_url == "https://perenual.com/storage/image/upgrade_access.jpg") {
+            image = "images/default.png";
+        } else {
+            image = plant?.default_image?.original_url;
+            
+        }
+        return image
+    }
+    
+
+
     return (
-        <div className="search-results">
-             <h1>Results Here</h1>
-             { apiSearchResult.map(plant => {
-                return(
-                    <Card key={plant?.id} sx={{ width: 350, minWidth: 200, display: 'flex', flexDirection: 'column'}} className="result-card">
-                        <CardMedia 
+        <div className="search-results-inside">
+            <h1>Results Here</h1>
+            {apiSearchResult.map(plant => {
+                return (
+
+                    <Card
+                        key={plant?.id}
+                        sx={{ width: 350, minWidth: 200, display: 'flex', flexDirection: 'column', gap: 1, margin: 3, padding: 2 }}
+                        className="result-card">
+                        <CardMedia
                             component='img'
-                            height='140'
-                            image={plant?.default_image?.original_url}
+                            height='340'
+                            image={plantImage(plant)}
                             alt={plant?.common_name}
                         />
                         <Typography>
                             {plant?.common_name}
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
+                        <Typography
+                            variant="body2"
+                            color="text.secondary">
                             {plant?.scientific_name}
                         </Typography>
                     </Card>
+
                 )
-             })}
+            })}
         </div>
-       
+
     )
 
 }
