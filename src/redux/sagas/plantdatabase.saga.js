7dcfb86c4af473a4d97debaa036305347fcdee63
Aubrey Graves <grav0165@gmail.com>
2023-08-01 11:18:55 -0500

    updating stack to include all currently created data. Working on plant database saga route currently

--- src/redux/sagas/plantdatabase.saga.js
+++ src/redux/sagas/plantdatabase.saga.js
@@ -1,1 +1,11 @@
-import axios from "axios"
+import axios from "axios";
+import { put, takeLatest } from 'redux-saga/effects'
+
+// Request to database to pull all currently stored plant information
+
+function fetchPlantList (action) {
+    try {
+        const plantListResponse = yield axios.get('/plantList')
+        yield put({ type: 'SET_PLANT_DATABASE', payload: plantListResponse.data.data})
+    }
+}
