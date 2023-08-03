8fd390d0082f40bcb9ca87358e511c3132bf9f82
Aubrey Graves <grav0165@gmail.com>
2023-08-02 20:55:10 -0500

    Created a function which measures the amount of time between two dates, looking at current and date taken from database for watering purposes

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -9,20 +9,14 @@ function ToDo() {
     // Adding store to run calculation
     let userPlantList = useSelector(store => store.userPlantDatabase.userPlantDatabaseResponse);
 
-    // Code that looks at current date, matches against database date
-
+    // Code that looks at current date, matches against database date 
     const toDoDay = (plant) => {
         let date_1 = new Date();
-        let date_2 = new Date(userPlantList?.[0]?.water_date)
-        let dateDifference = (date_1.getTime() - date_2.getTime()) / 1000 / 60 / 60 / 24
+        let date_2 = new Date(plant?.water_date)
+        // takes current date, minus the inputed date of the 
+        let dateDifference = ((date_1.getTime() - date_2.getTime()) / 1000 / 60 / 60 / 24) - plant?.water_days
         return dateDifference
     }
-    let date_1 = new Date();
-    let date_2 = new Date(userPlantList?.[0]?.water_date)
-    let different = date_1.getTime() - date_2.getTime();
-    console.log('The different between the dates is: ', different)
-    let dateDifference = different / 1000 / 60 / 60 / 24;
-    console.log('The days different between dates is: ', dateDifference)
 
 
 
@@ -32,7 +26,7 @@ function ToDo() {
     return (
         <div>
             <h3>To Do Today</h3>
-            <div>Dates</div>
+            <div>{toDoDay(userPlantList[0])}</div>
         </div>
     )
 }
