ed6781dbfb9fef1070c8369fce930316aee939fe
Aubrey Graves <grav0165@gmail.com>
2023-08-08 13:49:11 -0500

    added auth requirements to all three GET routes for the API requests

--- server/routes/api.router.js
+++ server/routes/api.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const axios = require('axios');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware')
 
 const perenualRouter = express.Router();
 
-perenualRouter.get('/details/:details', (req, res) => {
+perenualRouter.get('/details/:details', rejectUnauthenticated, (req, res) => {
     let searchQuery = req.params.details
     axios.get(`https://perenual.com/api/species/details/${searchQuery}?key=${process.env.PERENUAL_API_KEY}`)
     .then ( response => {
@@ -15,7 +16,7 @@ perenualRouter.get('/details/:details', (req, res) => {
     })
 })
 
-perenualRouter.get('/careDetails/:careDetails', (req, res) => {
+perenualRouter.get('/careDetails/:careDetails', rejectUnauthenticated, (req, res) => {
     let searchQuery = req.params.careDetails
     axios.get(`http://perenual.com/api/species-care-guide-list?species_id=${searchQuery}&key=${process.env.PERENUAL_API_KEY}`)
     .then ( response => {
@@ -26,7 +27,7 @@ perenualRouter.get('/careDetails/:careDetails', (req, res) => {
     })
 })
 
-perenualRouter.get('/search/:search', (req, res) => {
+perenualRouter.get('/search/:search', rejectUnauthenticated, (req, res) => {
     let searchQuery = req.params.search
     axios.get(`https://perenual.com/api/species-list?page=1&key=${process.env.PERENUAL_API_KEY}&q=${searchQuery}`)
     .then( response => {
