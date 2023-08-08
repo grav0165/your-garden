ee3d395350cb0496a52188fee7864e29fbfad3ab
Aubrey Graves <grav0165@gmail.com>
2023-08-08 13:13:21 -0500

    moved register button into the login box for the application

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -17,15 +17,7 @@ function LoginPage() {
       <LoginForm />
 
       <center>
-        <button
-          type="button"
-          className="btn btn_asLink register-button"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
+        
       </center>
       </div>
     </div>
